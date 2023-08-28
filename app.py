a9ce5a5709202efe314483755a9cced0e1c9a99c
xeven.ai <143064727+xevenai@users.noreply.github.com>
2023-08-28 11:44:30 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_chat import message
 from streamlit_Utilities import *
+# from instadev import *
+
 
 openapi_key = st.secrets["open_ai_key"]
 # openai.api_key = api_key.key
@@ -8,7 +10,6 @@ openai.api_key = openapi_key
 
 SerpAPIWrapper.serp_api_key = st.secrets["serp_api_key"]
 
-# from instadev import *
 if 'instagram' not in st.session_state:
     st.session_state['instagram'] = []
     st.session_state['instagramImage'] = []
@@ -35,26 +36,26 @@ if "button_clicked" not in st.session_state:
 def callback():
     st.session_state.button_clicked=True
 st.title('Content Generator Demo')
-prompt = st.text_input('Write your prompt to generate the social media content')
+prompt = st.text_input('Write Your Topic.')
 
 insta_button, twitter_button, facebook_button, linkedIn_button, blog_Title,blog_structure,blog_content,blog_image,blog_SEO,blog_Links = st.columns(10)
 
 if prompt:
     with insta_button:
-        if st.button("Instagram"):
+        if st.sidebar.button("Instagram"):
             st.session_state['instagram'] = generate_Instagram_content(prompt)
             instaRefineText=TextRefine(prompt)
             st.session_state['instagramImage']=generate_thumbnail_background(instaRefineText)
             # newRespone=TextRefine(prompt)
     with twitter_button:
-        if st.button("XTweet"):
+        if st.sidebar.button("Twitter"):
             st.session_state['twitter'] = generate_Twitter_content(prompt)
             twiitwerRefineText=TextRefine(prompt)
             st.session_state['twitterImage']=generate_thumbnail_background(twiitwerRefineText)
             # newRespone=TextRefine(prompt)
 
     with facebook_button:
-        if st.button("FaceBook"):
+        if st.sidebar.button("Facebook"):
             text= generate_Facebook_content(prompt)
             #removing left emojis
             st.session_state['facebook'] =remove_emojis(text)
@@ -62,51 +63,34 @@ if prompt:
             st.session_state['faceebokImage']=generate_thumbnail_background(fbRefineText)
             # newRespone=TextRefine(prompt)
     with linkedIn_button:
-        if st.button("LinkedIn"):
+        if st.sidebar.button("LinkedIn"):
             text=generate_LinkedIn_content(prompt)
             st.session_state['linkedin'] =remove_emojis(text)
             linkedinRefineText=TextRefine(prompt)
             st.session_state['linkedinImage']=generate_thumbnail_background(linkedinRefineText)
             # newRespone=TextRefine(prompt)
-    # with blog_Title:
-    #     if st.button("Blog Title   "):
-    #         st.session_state['blogTitle']=blogMultiTitleGenerator(prompt)
-    #         # response_dict = eval(st.session_state['blogTitle'])
-    # with blog_structure:
-    #     if st.button("Blog Strcuture        "):
-    #         st.session_state['blogStructure'] = generate_Blog_Structure(prompt)
-    # with blog_content:
-    #     if st.button("Blog Content       "):
-    #         st.session_state['blogContent'] = generate_Blog_Content(prompt, st.session_state['blogStructure'])
-    # with blog_SEO:
-    #     if st.button("Blog SEO        "):
-    #         st.session_state['blogSEO']=generate_Blog_SEO(prompt)
-    # with blog_image:
-    #     if st.button("Blog Image        "):
-    #         blogRefineText=blogMultiPromptGenerator(prompt,st.session_state['blogContent'])
-    #         st.session_state['blogImage']=generate_multi_thumbnail_background(blogRefineText)
-    # with blog_Links:
-    #     if st.button("Blog Links"):
-    #         blogLink=topic_generate(prompt)
-    #         st.session_state['blogLinks']=blog_repo_links(blogLink)
-if prompt:
     with blog_Title:
-        if st.button("Blog Title"):
+        if st.sidebar.button("Blog Title"):
             st.session_state['blogTitle']=blogMultiTitleGenerator(prompt)
             # response_dict = eval(st.session_state['blogTitle'])
     with blog_structure:
-        if st.button("Blog Strcuture"):
+        if st.sidebar.button("Blog Structure"):
             st.session_state['blogStructure'] = generate_Blog_Structure(prompt)
     with blog_content:
-        if st.button("Blog Content"):
+        if st.sidebar.button("Blog Content"):
             st.session_state['blogContent'] = generate_Blog_Content(prompt, st.session_state['blogStructure'])
     with blog_SEO:
-        if st.button("Blog SEO"):
+        if st.sidebar.button("Blog SEO"):
             st.session_state['blogSEO']=generate_Blog_SEO(prompt)
     with blog_image:
-        if st.button("Blog Image"):
+        if st.sidebar.button("Blog Image"):
             blogRefineText=blogMultiPromptGenerator(prompt,st.session_state['blogContent'])
             st.session_state['blogImage']=generate_multi_thumbnail_background(blogRefineText)
+    # with blog_Links:
+    #     if st.sidebar.button("Blog Links"):
+    #         blogLink=topic_generate(prompt)
+    #         st.session_state['blogLinks']=blog_repo_links(blogLink)
+
 
 if st.session_state['twitter']:
     st.header("Twitter Post Generated")
